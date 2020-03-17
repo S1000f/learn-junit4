7572556fbb0ff15197c8adab53467dbaac0a9f15
S1000f <dohp.kim@gmail.com>
2020-03-18 01:29:00 +0900

    first test

--- test/iloveyouboss/ScoreCollectionTest.java
+++ test/iloveyouboss/ScoreCollectionTest.java
@@ -1,14 +1,24 @@
 package iloveyouboss;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 import org.junit.Test;
 
 public class ScoreCollectionTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void answerArithmeticMeanOfTwoNumbers() {
+		// Arrange
+		ScoreCollection collection = new ScoreCollection();
+		collection.add(()-> 5);
+		collection.add(()-> 7);
+		
+		// Act
+		int expected = collection.arithmeticMean();
+		
+		// Assert
+		assertThat(expected, equalTo(6));
 	}
 
 }
