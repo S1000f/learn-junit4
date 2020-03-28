8fbbcacaa00610c7f0af3bfe320da8d4a4fbe1f9
dohpkim <dohp.kim@gmail.com>
2020-03-28 16:08:01 +0900

    practice Parameterized Test Runner

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,24 +1,19 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.assertEquals;
+
 public class CalculatorTest {
-    private int nbErrors = 0;
+    @Test
     public void testAdd() {
         Calculator cal = new Calculator();
         double result = cal.add(10, 50);
-        if (result != 60) {
-            throw new IllegalStateException("Bad result: " + result);
-        }
+        assertEquals(60, result, 0);
     }
 
-    public static void main(String[] args) {
-        CalculatorTest test = new CalculatorTest();
-        try {
-            test.testAdd();
-        } catch (Throwable e) {
-            test.nbErrors++;
-            e.printStackTrace();
-        }
-
-        if(test.nbErrors > 0) {
-            throw new IllegalStateException("There were " + test.nbErrors + " errors");
-        }
-    }
 }
+
