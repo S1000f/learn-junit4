aca8538d85f9464f42b32a8642fc0636a1a9c646
dohpkim <dohp.kim@gmail.com>
2020-04-20 23:45:41 +0900

    ch06 start

--- src/my/junit/ch06/JettySample.java
+++ src/my/junit/ch06/JettySample.java
@@ -1,11 +1,17 @@
 package my.junit.ch06;
 
-import org.eclipse.jetty.*;
-
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 public class JettySample {
     public static void main(String[] args) throws Exception {
+        Server server = new Server(8080);
 
+        ContextHandler root = new ContextHandler(server,"/");
+        root.setResourceBase("./pom.xml");
+        root.setHandler(new ResourceHandler());
 
+        server.start();
     }
 }
