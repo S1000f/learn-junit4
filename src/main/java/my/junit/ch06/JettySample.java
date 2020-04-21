9be447b43a0017d46d239db45361cb2731c7b3b0
dohpkim <dohp.kim@gmail.com>
2020-04-22 00:04:56 +0900

    convert to Maven project

--- src/main/java/my/junit/ch06/JettySample.java
+++ src/main/java/my/junit/ch06/JettySample.java
@@ -1,14 +1,15 @@
 package my.junit.ch06;
 
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandler;
-import org.eclipse.jetty.server.handler.ResourceHandler;
+
+import org.mortbay.jetty.Server;
+import org.mortbay.jetty.handler.ResourceHandler;
+import org.mortbay.jetty.servlet.Context;
 
 public class JettySample {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
 
-        ContextHandler root = new ContextHandler(server,"/");
+        Context root = new Context(server,"/");
         root.setResourceBase("./pom.xml");
         root.setHandler(new ResourceHandler());
 
